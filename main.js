8acee3388e09a651b72a0b2193d15666929c58c3
felipe.miranda.pires@escola.pr.gov.br <felipe.miranda.pires@escola.pr.gov.br>
2022-04-26 18:46:44 -0300

    historia uodate

---
+++ main.js
@@ -0,0 +1,40 @@
+
+/* historia aulas */
+const aulahis1= document.querySelector("#ah1");
+const aulahis2 = document.querySelector('#ah6');
+const aulahis3 = document.querySelector('#ah6');
+const aulahis4 = document.querySelector('#ah6');
+const aulahis5 = document.querySelector('#ah6');
+const aulahis6 = document.querySelector('#ah6');
+const aulahis7 = document.querySelector('#ah7');
+const aulahis8 = document.querySelector('#ah6');
+
+/* historia questões */
+
+const queshis1 = 
+
+
+/* Atualizar aulas historia */
+
+aulahis1.href = "https://google.com.br";
+aulahis2.href = "https://google.com.br";
+aulahis3.href = "https://google.com.br";
+aulahis4.href = "https://google.com.br";
+aulahis5.href = "https://google.com.br";
+aulahis6.href = "https://google.com.br";
+aulahis7.href = "https://google.com.br";
+
+
+
+/* Geografia aulas */ 
+const aulageo1 = document.querySelector('#ah6');
+const aulageo2 = document.querySelector('#ah6');
+const aulageo3 = document.querySelector('#ah6');
+const aulageo4 = document.querySelector('#ah6');
+const aulageo5 = document.querySelector('#ah6');
+const aulageo6 = document.querySelector('#ah6');
+const aulageo7 = document.querySelector('#ah6');
+const aulageo8 = document.querySelector('#ah6');
+
+
+
