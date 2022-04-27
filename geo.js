7f0fa20821da0d34beefb76720c748b5af57025f
felipe.miranda.pires@escola.pr.gov.br <felipe.miranda.pires@escola.pr.gov.br>
2022-04-27 16:19:58 -0300

    geo

--- geo.js
+++ geo.js
@@ -24,22 +24,22 @@ const questgeo8 = document.querySelector("#qg8");
 
 
 /* Atualizar geografia */
-aulageo1.href = "https://google.com.br";
-aulageo2.href = "https://google.com.br";
-aulageo3.href = "https://google.com.br";
-aulageo4.href = "https://google.com.br";
-aulageo5.href = "https://google.com.br";
-aulageo6.href = "https://google.com.br";
-aulageo7.href = "https://google.com.br";
-aulageo8.href = "https://google.com.br";
+aulageo1.href = "https://docs.google.com/presentation/d/1rhQV1t3CxS0-IjLm6hnlZatWfli-jmYk/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulageo2.href = "https://docs.google.com/presentation/d/1w3zEY8MnV1Lo0jBE_TKBFlYv3-ek8NFy/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulageo3.href = "https://docs.google.com/presentation/d/1u8lHBvShHrBVtAw0pe6lvydJ5XpfcTak/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulageo4.href = "https://docs.google.com/presentation/d/134vITPjxMnyS_OUQdzPmFiqs52G4xdmz/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulageo5.href = "https://docs.google.com/presentation/d/1OFPq7DL69AoQxTpNrSzXAW9pYFzqwDTE/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulageo6.href = "https://docs.google.com/presentation/d/1koBVYEDVw6lPqp3MiV0ygoyO1GLr9SVE/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulageo7.href = " ";
+aulageo8.href = " ";
 
 /* questões */ 
 
-questgeo1.href = "https://google.com.br";
-questgeo2.href = "https://google.com.br";
-questgeo3.href = "https://google.com.br";
-questgeo4.href = "https://google.com.br";
-questgeo5.href = "https://google.com.br";
-questgeo6.href = "https://google.com.br";
-questgeo7.href = "https://google.com.br";
-questgeo8.href = "https://google.com.br";
+questgeo1.href = "https://docs.google.com/document/d/155xV1rtFgR2g_vGOKh8ZcpQE51Ux37v0/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+questgeo2.href = "https://docs.google.com/document/d/1l0yBPZ0RtL-prZyRYxV5alMKEO7XXQQh/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+questgeo3.href = "https://docs.google.com/document/d/17VjsHrkkWDbBNAuJq01iNese6oGsKeGp/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+questgeo4.href = "https://docs.google.com/document/d/1KCttXEGxsqgLj78z4OwLtQIKSKS-U2m6/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+questgeo5.href = "https://docs.google.com/document/d/1QN_IK_rR5awnXgMXiu0EUAeFCgk3c69S/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+questgeo6.href = "https://docs.google.com/document/d/1E_r0itm-Fu4PVv-M0wfgUmVwWR9FGyqg/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+questgeo7.href = " ";
+questgeo8.href = " "
--- his.js
+++ his.js
@@ -23,24 +23,24 @@ const questhist8 = document.querySelector("#qh8");
 
 /* Atualizar historia */
 
-aulahis1.href = "https://google.com.br";
-aulahis2.href = "https://google.com.br";
-aulahis3.href = "https://google.com.br";
-aulahis4.href = "https://google.com.br";
-aulahis5.href = "https://google.com.br";
-aulahis6.href = "https://google.com.br";
-aulahis7.href = "https://google.com.br";
-aulahis8.href = "https://google.com.br";
+aulahis1.href = "https://drive.google.com/file/d/1pSRG-pc-BOizRTCoQfuvngUClASBXfIQ/view?usp=drivesdk";
+aulahis2.href = "https://docs.google.com/presentation/d/1o91qerYezRI0JDT0af0SEStRKK1saaoj/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulahis3.href = "https://docs.google.com/presentation/d/19LAkreVv3XOXsR7UL-6B9zmXtFT2fsqY/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulahis4.href = "https://docs.google.com/presentation/d/1tayvcjNIPNpkG8kK-ZLz2_fUlWufS1_W/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulahis5.href = "https://docs.google.com/presentation/d/1c4vtqCIXIt9lZyu8m3OCS_6XZqP6s5Hw/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulahis6.href = "https://docs.google.com/presentation/d/1YQ92WIjD-w2edmwKjY0Lo91oB2niYzhZ/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulahis7.href = "https://docs.google.com/presentation/d/1m6ujnEg61zeh7dxPbN4jaNT5IFmPi9Gd/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
+aulahis8.href = "https://drive.google.com/file/d/1seQPkCdogM-3-4W6B_D6H4CMH-apAd_3/view?usp=drivesdk";
 
 /* questões */
-questhist1.href = "https://google.com.br";
-questhist2.href = "https://google.com.br";
-questhist3.href = "https://google.com.br";
-questhist4.href = "https://google.com.br";
-questhist5.href = "https://google.com.br";
-questhist6.href = "https://google.com.br";
-questhist7.href = "https://google.com.br";
-questhist8.href = "https://google.com.br";
+questhist1.href = "";
+questhist2.href = "https://docs.google.com/document/d/14u_EvKXu45onNnrjobezFdzmxoIZuQTNojg0oM6-aS0/edit?usp=drivesdk";
+questhist3.href = "https://docs.google.com/document/d/14Qg91plzgqKttRYEbg749MZkwM1VGI4b2NUicK1OMs4/edit?usp=drivesdk";
+questhist4.href = "https://docs.google.com/document/d/1qsht2pLOa_7HQjm0jOIZzq1DIkj4hbf0LHnaTJrURkE/edit?usp=drivesdk";
+questhist5.href = "https://docs.google.com/document/d/1ZBOOp4BgU6mxTxlgvYI48NcH1ZjXMDurhBSmF5YW0ZM/edit?usp=drivesdk";
+questhist6.href = "https://docs.google.com/document/d/1JSVHCV8N20VMvbsKbinFNwlnJ9k1JGsw512iVkmX6MY/edit?usp=drivesdk";
+questhist7.href = "https://docs.google.com/document/d/1LdOqMXpL3btTi8b8mte08cHsLPW5RK6TODA_mRbnS4E/edit?usp=drivesdk";
+questhist8.href = " ";
 
 
 
