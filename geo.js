07ad14a999d68ce19f7a0eb0c20aba162cda9f48
Pires-Felipe <felipe.miranda.pires@escola.pr.gov.br>
2022-04-27 13:45:21 -0300

    geo

---
+++ geo.js
@@ -0,0 +1,45 @@
+
+
+
+/* Geografia aulas */ 
+const aulageo1 = document.querySelector("#ag1");
+const aulageo2 = document.querySelector('#ag2');
+const aulageo3 = document.querySelector('#ag3');
+const aulageo4 = document.querySelector('#ag4');
+const aulageo5 = document.querySelector('#ag5');
+const aulageo6 = document.querySelector('#ag6');
+const aulageo7 = document.querySelector('#ag7');
+const aulageo8 = document.querySelector('#ag8');
+
+/* Geografia questões */
+const questgeo1 = document.querySelector("#qg1");
+const questgeo2 = document.querySelector("#qg2");
+const questgeo3 = document.querySelector("#qg3");
+const questgeo4 = document.querySelector("#qg4");
+const questgeo5 = document.querySelector("#qg5");
+const questgeo6 = document.querySelector("#qg6");
+const questgeo7 = document.querySelector("#qg7");
+const questgeo8 = document.querySelector("#qg8");
+
+
+
+/* Atualizar geografia */
+aulageo1.href = "https://google.com.br";
+aulageo2.href = "https://google.com.br";
+aulageo3.href = "https://google.com.br";
+aulageo4.href = "https://google.com.br";
+aulageo5.href = "https://google.com.br";
+aulageo6.href = "https://google.com.br";
+aulageo7.href = "https://google.com.br";
+aulageo8.href = "https://google.com.br";
+
+/* questões */ 
+
+questgeo1.href = "https://google.com.br";
+questgeo2.href = "https://google.com.br";
+questgeo3.href = "https://google.com.br";
+questgeo4.href = "https://google.com.br";
+questgeo5.href = "https://google.com.br";
+questgeo6.href = "https://google.com.br";
+questgeo7.href = "https://google.com.br";
+questgeo8.href = "https://google.com.br";
---
+++ his.js
@@ -0,0 +1,48 @@
+var historia = document.querySelector("#historia");
+
+	
+/* historia aulas */
+const aulahis1 = document.querySelector('#ah1');
+const aulahis2 = document.querySelector('#ah2');
+const aulahis3 = document.querySelector('#ah3');
+const aulahis4 = document.querySelector('#ah4');
+const aulahis5 = document.querySelector('#ah5');
+const aulahis6 = document.querySelector('#ah6');
+const aulahis7 = document.querySelector('#ah7');
+const aulahis8 = document.querySelector('#ah8');
+
+/* historia questões */
+const questhist1 = document.querySelector("#qh1");
+const questhist2 = document.querySelector("#qh2");
+const questhist3 = document.querySelector("#qh3");
+const questhist4 = document.querySelector("#qh4");
+const questhist5 = document.querySelector("#qh5");
+const questhist6 = document.querySelector("#qh6");
+const questhist7 = document.querySelector("#qh7");
+const questhist8 = document.querySelector("#qh8");
+
+/* Atualizar historia */
+
+aulahis1.href = "https://google.com.br";
+aulahis2.href = "https://google.com.br";
+aulahis3.href = "https://google.com.br";
+aulahis4.href = "https://google.com.br";
+aulahis5.href = "https://google.com.br";
+aulahis6.href = "https://google.com.br";
+aulahis7.href = "https://google.com.br";
+aulahis8.href = "https://google.com.br";
+
+/* questões */
+questhist1.href = "https://google.com.br";
+questhist2.href = "https://google.com.br";
+questhist3.href = "https://google.com.br";
+questhist4.href = "https://google.com.br";
+questhist5.href = "https://google.com.br";
+questhist6.href = "https://google.com.br";
+questhist7.href = "https://google.com.br";
+questhist8.href = "https://google.com.br";
+
+
+
+
+
