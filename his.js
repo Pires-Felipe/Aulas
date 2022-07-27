16f5ea2359102db66644915433af34f60c0f7c68
Felipe Miranda Pires <102832296+Pires-Felipe@users.noreply.github.com>
2022-07-27 15:48:32 -0300

    Add files via upload

--- his.js
+++ his.js
@@ -10,8 +10,17 @@ const aulahis5 = document.querySelector('#ah5');
 const aulahis6 = document.querySelector('#ah6');
 const aulahis7 = document.querySelector('#ah7');
 const aulahis8 = document.querySelector('#ah8');
+const aulahis9 = document.querySelector('#ah9');
+const aulahis10 = document.querySelector('#ah10');
+const aulahis11 = document.querySelector('#ah11');
+const aulahis12 = document.querySelector('#ah12');
+const aulahis13 = document.querySelector('#ah13');
+const aulahis14 = document.querySelector('#ah14');
+const aulahis15 = document.querySelector('#ah15');
+const aulahis16 = document.querySelector('#ah16');
 
 /* historia questões */
+
 const questhist1 = document.querySelector("#qh1");
 const questhist2 = document.querySelector("#qh2");
 const questhist3 = document.querySelector("#qh3");
@@ -20,29 +29,56 @@ const questhist5 = document.querySelector("#qh5");
 const questhist6 = document.querySelector("#qh6");
 const questhist7 = document.querySelector("#qh7");
 const questhist8 = document.querySelector("#qh8");
-
-/* Atualizar historia */
-
-aulahis1.href = "https://drive.google.com/file/d/1pSRG-pc-BOizRTCoQfuvngUClASBXfIQ/view?usp=drivesdk";
-aulahis2.href = "https://docs.google.com/presentation/d/1o91qerYezRI0JDT0af0SEStRKK1saaoj/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
-aulahis3.href = "https://docs.google.com/presentation/d/19LAkreVv3XOXsR7UL-6B9zmXtFT2fsqY/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
-aulahis4.href = "https://docs.google.com/presentation/d/1tayvcjNIPNpkG8kK-ZLz2_fUlWufS1_W/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
-aulahis5.href = "https://docs.google.com/presentation/d/1c4vtqCIXIt9lZyu8m3OCS_6XZqP6s5Hw/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
-aulahis6.href = "https://docs.google.com/presentation/d/1YQ92WIjD-w2edmwKjY0Lo91oB2niYzhZ/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
-aulahis7.href = "https://docs.google.com/presentation/d/1m6ujnEg61zeh7dxPbN4jaNT5IFmPi9Gd/edit?usp=drivesdk&ouid=102453837299334610632&rtpof=true&sd=true";
-aulahis8.href = "https://drive.google.com/file/d/1seQPkCdogM-3-4W6B_D6H4CMH-apAd_3/view?usp=drivesdk";
-
-/* questões */
-questhist1.href = "";
-questhist2.href = "https://docs.google.com/document/d/14u_EvKXu45onNnrjobezFdzmxoIZuQTNojg0oM6-aS0/edit?usp=drivesdk";
-questhist3.href = "https://docs.google.com/document/d/14Qg91plzgqKttRYEbg749MZkwM1VGI4b2NUicK1OMs4/edit?usp=drivesdk";
-questhist4.href = "https://docs.google.com/document/d/1qsht2pLOa_7HQjm0jOIZzq1DIkj4hbf0LHnaTJrURkE/edit?usp=drivesdk";
-questhist5.href = "https://docs.google.com/document/d/1ZBOOp4BgU6mxTxlgvYI48NcH1ZjXMDurhBSmF5YW0ZM/edit?usp=drivesdk";
-questhist6.href = "https://docs.google.com/document/d/1JSVHCV8N20VMvbsKbinFNwlnJ9k1JGsw512iVkmX6MY/edit?usp=drivesdk";
-questhist7.href = "https://docs.google.com/document/d/1LdOqMXpL3btTi8b8mte08cHsLPW5RK6TODA_mRbnS4E/edit?usp=drivesdk";
-questhist8.href = " ";
+const questhist9 = document.querySelector("#qh9");
+const questhist10 = document.querySelector("#qh10");
+const questhist11 = document.querySelector("#qh11");
+const questhist12 = document.querySelector("#qh12");
+const questhist13 = document.querySelector("#qh13");
+const questhist14 = document.querySelector("#qh14");
+const questhist15 = document.querySelector("#qh15");
+const questhist16= document.querySelector("#qh16");
 
 
 
 
 
+/* Atualizar aulas historia */
+
+aulahis1.href = "https://github.com/"
+aulahis2.href = 
+aulahis3.href = 
+aulahis4.href = 
+aulahis5.href = 
+aulahis6.href = 
+aulahis7.href = 
+aulahis8.href = 
+aulahis9.href = 
+aulahis10.href = 
+aulahis11.href = 
+aulahis12.href = 
+aulahis13.href = 
+aulahis14.href = 
+aulahis15.href = 
+aulahis16.href = 
+
+
+
+/* atualizar questões historia */
+
+questhist1.href = "google.com"
+questhist2.href = 
+questhist3.href = 
+questhist4.href = 
+questhist4.href = 
+questhist5.href = 
+questhist6.href = 
+questhist7.href = 
+questhist8.href = 
+questhist9.href = 
+questhist10.href = 
+questhist11.href = 
+questhist12.href = 
+questhist13.href = 
+questhist14.href = 
+questhist15.href = 
+questhist16.href = 
